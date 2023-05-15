55ab96b06ab8ec29bc1e77014b8e34d315be7b66
FerozShaik96 <ferozshaik1997@gmail.com>
2023-05-15 23:29:52 +0530

    Setting Css Classes Dynamically

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -6,11 +6,11 @@ const Button = (props) => {
   return (
     <button
       type={props.type}
-      className="button"
-      style={{
-        background: !props.valid ? "#FFCCCB" : "#ac0e77",
-        borderColor: !props.valid ? "#FFCCCB" : "#ac0e77",
-      }}
+      className={`button ${!props.valid ? "invalid" : " "}`}
+      // style={{
+      //   background: !props.valid ? "#FFCCCB" : "#ac0e77",
+      //   borderColor: !props.valid ? "#FFCCCB" : "#ac0e77",
+      // }}
       onClick={props.onClick}
     >
       {props.children}
